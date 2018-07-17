b06410936fe2d8015b8c382156a46d9970168585
uday <uday@192.168.0.40>
2018-07-18 00:30:52 +0530

    my first commit into GitHub

---
+++ RGB-beans/src/main/java/com/rgb/beans/Account.java
@@ -0,0 +1,69 @@
+package com.rgb.beans;
+
+public class Account {
+
+	private Integer accountId;
+	private String accountNumber;
+	private Integer branchId;
+	private Integer cutomerId;
+	private Double balance;
+	private Integer accountTypeId;
+	private Byte status;
+
+	public Integer getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(Integer accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public Integer getBranchId() {
+		return branchId;
+	}
+
+	public void setBranchId(Integer branchId) {
+		this.branchId = branchId;
+	}
+
+	public Integer getCutomerId() {
+		return cutomerId;
+	}
+
+	public void setCutomerId(Integer cutomerId) {
+		this.cutomerId = cutomerId;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Integer getAccountTypeId() {
+		return accountTypeId;
+	}
+
+	public void setAccountTypeId(Integer accountTypeId) {
+		this.accountTypeId = accountTypeId;
+	}
+
+	public Byte getStatus() {
+		return status;
+	}
+
+	public void setStatus(Byte status) {
+		this.status = status;
+	}
+
+}
---
+++ RGB-beans/src/main/java/com/rgb/beans/RgbBeansApplication.java
@@ -0,0 +1,12 @@
+package com.rgb.beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgbBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgbBeansApplication.class, args);
+	}
+}
