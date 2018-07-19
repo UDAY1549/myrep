5656f223fdfc355529b7086101cb6b6f43fbff32
uday <uday@192.168.0.27>
2018-07-19 16:27:30 +0530

    TL 1 COMMIT

--- RGB-beans/src/main/java/com/rgb/beans/AccountType.java
+++ RGB-beans/src/main/java/com/rgb/beans/AccountType.java
@@ -4,5 +4,20 @@ public class AccountType {
 	 
 	Integer SavingAccount;
 	Integer CurrentAccount;
+	
+	public Integer getSavingAccount() {
+		return SavingAccount;
+	}
+	public void setSavingAccount(Integer savingAccount) {
+		SavingAccount = savingAccount;
+	}
+	public Integer getCurrentAccount() {
+		return CurrentAccount;
+	}
+	public void setCurrentAccount(Integer currentAccount) {
+		CurrentAccount = currentAccount;
+	}
+	
+	
 
 }
